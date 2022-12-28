76a8067d178b37c17ac40ab12be25486a9464005
Amado <kisik854@gmail.com>
2022-12-28 23:21:52 +1000

    basket debug

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -2,64 +2,50 @@ import React, {useEffect, useState} from 'react';
 import './Basket.css'
 import {useAppSelector} from "../App/hooks";
 import axios, {AxiosResponse} from "axios";
-
-interface Product {
-    title: string,
-    price: number,
-    category: string,
-    image: string,
-    id: number,
-    description: string,
-    rating: {rate: number, count: number}
-}
+import {IFullProduct} from "../../interfaces";
 
 function Basket() {
     const idProducts = useAppSelector((state) => state.basket.idProducts)
 
-    let [basket, setBasket] = useState<Product[]>([])
-
-    const getProduct =  async () => {
-        let datas: Product[] = []
-        for(let i = 0; i < idProducts.length; i++) {
-            await axios.get(`https://fakestoreapi.com/products/${idProducts[i]}`)
-                .then ((response: AxiosResponse) => {
-                    let data: Product = response.data
-                    datas.push(data)
-                })
-                .catch(err => {
-                    console.log(err.message)
-                })
-        }
-        setBasket(datas)
+    let [basket, setBasket] = useState<IFullProduct[]>([])
+    const getProduct =  async (id: number) => {
+            if (basket.length < 1) {
+                await axios.get(`https://fakestoreapi.com/products/${id}`)
+                    .then ((response: AxiosResponse) => {
+                        let product: IFullProduct = response.data
+                        setBasket(prev =>  [...prev, product])
+                    })
+                    .catch( err => {
+                        console.log(err.message)
+                    })
+            }
     }
 
-    useEffect(() => {
-        if (idProducts) {
-            getProduct()
-        }
-    })
+        getProduct(5)
+
 
     const createBasketItems = () => {
-        if (basket.length > 0) {
-            let products: JSX.Element[] = []
-            products = basket.map((item: Product) => {
+        if (basket.length > 1) {
+            return basket.map((item: IFullProduct) => {
                 return (
-                    <div className="basket__item">
+                    <div className="basket__item" key={item.id}>
                         {item.title}
                     </div>
                 )
             })
-            return products
         } else {
-            return 'Ой, пустенько'
+            return []
         }
     }
+
+    let productList: JSX.Element[]  = createBasketItems()
+
 
     return (
         <div className='basket'>
             <div className="container">
                 <div className="basket__inner">
-                    {createBasketItems()}
+                    {productList}
                 </div>
             </div>
         </div>
