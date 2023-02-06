cb8ce9712a4bc588f7bb5fa0a5da3156341f1f73
Amado <kisik854@gmail.com>
2023-02-06 22:51:42 +1000

    create component payNavigation

---
+++ src/components/Basket/PayNavigation/PayNavigation.tsx
@@ -0,0 +1,25 @@
+import React, {Dispatch, SetStateAction, useState} from 'react';
+import './PayNavigation.css'
+
+function PayNavigation(props: {amount: number, showCheckboxes: Dispatch<SetStateAction<string>>, checkboxStyle: string}) {
+    let [buttonValue, setButtonValue] = useState('choose to buy')
+    let showChoice = () => {
+        if (props.checkboxStyle === 'none') {
+            props.showCheckboxes('block')
+            setButtonValue('cancel selection')
+        } else {
+            props.showCheckboxes('none')
+            setButtonValue('choose to buy')
+        }
+    }
+
+    return (
+        <div className='payNav'>
+            <button className={'payNav__btn'} onClick={showChoice}>{buttonValue}</button>
+            <button className={'payNav__btn'}>buy everything</button>
+            { props.amount ? <div className={'payNav__amount'}>amount payable: <span>{props.amount}$</span></div> : '' }
+        </div>
+    );
+}
+
+export default PayNavigation;
